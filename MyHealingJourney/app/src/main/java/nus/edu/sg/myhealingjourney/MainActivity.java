1f6f803bce2c309df46be40d6e159864249693d1
Liting099 <55421176+Liting099@users.noreply.github.com>
2019-09-18 12:09:39 +0800

    Add files via upload

---
+++ MyHealingJourney/app/src/main/java/nus/edu/sg/myhealingjourney/MainActivity.java
@@ -0,0 +1,14 @@
+package nus.edu.sg.myhealingjourney;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
